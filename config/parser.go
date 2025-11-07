3f41b54903ed096f82b8d40f1c91c4f17cc0b4c4
Luciano Taurino <luciano@taurino.eu>
2025-11-07 08:53:20 +0100

    base configuration added WIP

--- config/parser.go
+++ config/parser.go
@@ -1,1 +1,68 @@
 package config
+
+import (
+	"fmt"
+	"os"
+
+	"strconv"
+
+	"github.com/joho/godotenv"
+	"github.com/pelletier/go-toml/v2"
+)
+
+// Path to the default configuration TOML file
+const defaultConfigFilePath = "config/default.toml"
+
+var config AppConfig
+
+func GetConfig() AppConfig {
+	return config
+}
+
+func LoadConfig() error {
+	err := godotenv.Load()
+	if err != nil {
+		return fmt.Errorf("cannot load config: '%w'", err)
+	}
+
+	return nil
+}
+
+func newConfigWithDefaults() (AppConfig, error) {
+	var defaultConfig AppConfig
+
+	data, err := os.ReadFile(defaultConfigFilePath)
+	if err != nil {
+		return AppConfig{}, fmt.Errorf("could not read default config file '%s': %w", defaultConfigFilePath, err)
+	}
+
+	err = toml.Unmarshal(data, &defaultConfig)
+	if err != nil {
+		return AppConfig{}, fmt.Errorf("error unmarshalling default config from '%s': %w", defaultConfigFilePath, err)
+	}
+	return defaultConfig, nil
+}
+
+func updateConfigFromEnv(cfg *AppConfig) error {
+	if portStr := os.Getenv("GIN_PORT"); portStr != "" {
+		port, err := strconv.Atoi(portStr)
+		if err != nil {
+			return fmt.Errorf("invalid GIN_PORT value: %w", err)
+		}
+		cfg.Gin.Port = port
+	}
+
+	if debugStr := os.Getenv("GIN_DEBUG"); debugStr != "" {
+		debug, err := strconv.ParseBool(debugStr)
+		if err != nil {
+			return fmt.Errorf("invalid GIN_DEBUG value: %w", err)
+		}
+		cfg.Gin.Debug = debug
+	}
+
+	if file := os.Getenv("SQLITE_FILE"); file != "" {
+		cfg.Sqlite.File = file
+	}
+
+	return nil
+}
--- config/types.go
+++ config/types.go
@@ -1,1 +1,17 @@
 package config
+
+type (
+	AppConfig struct {
+		Gin    GinConfig
+		Sqlite SqliteConfig
+	}
+
+	GinConfig struct {
+		Port  int
+		Debug bool
+	}
+
+	SqliteConfig struct {
+		File string
+	}
+)
